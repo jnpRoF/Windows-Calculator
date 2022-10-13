7bba723fd688d9e447f02480cad544df84193e9a
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-13 10:51:40 +0100

    scaffold basic components

--- src/components/Calculator/Calculator2.jsx
+++ src/components/Calculator/Calculator2.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import Navigation from "../Navigation/Navigation";
+import Mode from "../Mode/Mode";
 
 class Calculator2 extends Component {
   state = {};
 
   render() {
-    return <div></div>;
+    return (
+      <div className="calculator">
+        <div className="calc_body">
+          <Navigation />
+          <Mode />
+        </div>
+        <div className="calc-nav"></div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Mode/Mode.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Mode extends Component {
+  state = {};
+  render() {
+    return <div>Mode</div>;
+  }
+}
+
+export default Mode;
