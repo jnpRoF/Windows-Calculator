88936d53000b6d23015600c00c0f88777bffb602
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-14 21:39:52 +0100

    feat: initial commit for this branch

--- src/components/Calculator/Calculator2.jsx
+++ src/components/Calculator/Calculator2.jsx
@@ -140,7 +140,13 @@ class Calculator2 extends Component {
   };
 
   handleResult = () => {
-    this.setState({ result: this.state.currentOperation.firstOperand });
+    this.setState({
+      result: this.calculate(
+        this.state.currentOperation.operator,
+        +this.state.currentOperation.firstOperand,
+        +this.state.currentOperation.secondOperand
+      ),
+    });
   };
 
   render() {
